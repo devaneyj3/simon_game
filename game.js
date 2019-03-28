dd9b854ab21b8b798cc75740daa4c9dc2ce12b8d
Jordan Devaney <jordandevaney28@gmail.com>
2019-03-28 15:33:24 -0400

    add animation to button press

--- game.js
+++ game.js
@@ -1,16 +1,44 @@
-
-let buttonColors = ["red", "blue", "green", "yellow"];
+let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
+$(".btn").click(function() {
+
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
 
 function nextSequence() {
-  let randomNumber = Math.floor(Math.random(3) * 4);
+    var randomNumber = Math.floor(Math.random() * 4);
 
-  let randomColorChoser = buttonColors[randomNumber];
-   
-  gamePattern.push(randomColorChoser);
+    let randomColourChoser = buttonColours[randomNumber];
 
+    gamePattern.push(randomColourChoser);
+
+  $("#" + randomColourChoser).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomColourChoser);
+  animatePress(randomColourChoser);
+}
+
+function playSound(name) {
+    
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+    
+}
+
+function animatePress(currentColor) {
+
+    $("." + currentColor).addClass("pressed");
+
+    setTimeout( function() {
+        $("." + currentColor).removeClass("pressed");
+    }, 100);
 }
 
 nextSequence();
